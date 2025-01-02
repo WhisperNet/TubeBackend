11bdf2c28093216c995e796780ccd7f9a689fdbd
Ridowan Sikder <ridowan.cse@gmail.com>
2025-01-02 23:45:07 +0600

    Express app setup with utils

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
-const test = () => {
-    console.log('test')
-console.log("test")
-  }
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    credentials: true
+}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true , limit: "16kb"}));
+app.use(express.static("public"));
+
+export default app;
